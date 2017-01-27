29375918c4117f876e5b826f740ce63a30de0156
YvanGuidoin <yvan.guidoin@gmail.com>
2017-01-27 23:25:35 +0100

    Custom webpack file

---
+++ site/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.bundle.js',
+    pathinfo: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react'],
+          plugins: ['transform-runtime']
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({ sourcemap: false }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico'
+    }),
+    // new webpack.HotModuleReplacementPlugin()
+  ],
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+}
